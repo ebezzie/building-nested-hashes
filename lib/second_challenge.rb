8669f1b7bfb500ab2db3ebd007c5604edf7e24ee
Ebony <ebony.furr@yahoo.com>
2020-10-30 20:22:26 -0500

    commit tree 6bda7360fced3dafee81213e8f6998a95cf3c066

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,10 +1,13 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
-      }, 
-    :capulet => {
-      #your key/value pairs here!
+    :montague => {:patriarch => {},
+      :matriarch => {},
+      :hero => {},                    #your key/value pairs here!
+      :hero_friends => [] }, 
+    :capulet => {:patriarch => {},
+      :matriarch => {},
+      :heroine => {},                            #your key/value pairs here!
+      :heroine_friends => []
       }}
 
   
